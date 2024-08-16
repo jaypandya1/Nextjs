b51351f21b6eec7d9abd60772bfe151d4a89ff1c
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2024-08-16 16:41:05 -0400

    dbconnect

---
+++ mstrymessage/src/lib/dbConnect.ts
@@ -0,0 +1,37 @@
+import mongoose, { Connection } from "mongoose";
+
+
+
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+
+const connection: ConnectionObject = {
+
+}
+
+
+async function dbConnect(): Promise<void>{
+    if (connection.isConnected) {
+        console.log("Already Connected to database");
+        return
+    }
+
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_URI || '',{})
+
+
+        connection.isConnected = db.connection[0].
+        readyState
+
+        console.log("DB connected Successfully");
+
+    } catch(error) {
+console.log("Database connection failed ", error);
+process.exit(1)
+    }
+}
+
+export default dbConnect;

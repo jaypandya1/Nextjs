f1c03aebf0ec30e6722a73e651e93bd176a1a70e
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2024-08-16 16:09:44 -0400

    zod

--- mstrymessage/src/model/user.ts
+++ mstrymessage/src/model/user.ts
@@ -18,27 +18,56 @@ const MessageSchema: Schema<Message> = new Schema({
             required: true,
             default: Date.now
         })
-
-        
         export interface User extends Document{
             username: string;
             email: string;
             password: string;
             verifyCode: string;
             verifyCodeExpiry: Date;
+            isVerified: boolean;
             isAcceptingMessage: boolean;
             Message: Message[];
 
         }
 
-        const UserSchema: Schema<user> = new Schema({
-
+        const UserSchema: Schema<User> = new Schema({
 
            username:{
                 type: String,
-                required: true,} ,
-                createAt: {
-                    type: Date,
-                    required: true,
-                    default: Date.now
-                })
+                required: [true,"Username is required"],
+                 trim:true,
+                 unique:true,
+           },
+
+           email:{
+            type: String,
+            required:[true,"Email is required "],
+            unique:true,
+            match:[^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$,'please use a valid email address']
+           },
+
+           password:{
+            type:String,
+            required: [true,"Password is required "],
+
+           },
+           verifyCode:{
+            type:String,
+            required: [true,"Verify code is required"]
+           }
+           ,
+           verifyCodeExpiry:{
+            type:String,
+            required: [true,"Verify Code Expiry is required "]
+           }
+           ,
+           isVerified:{
+            type:Boolean,
+            default:false,
+           },
+           isAcceptingMessage:{
+            type:Boolean,
+            default:true,
+           },
+           Messages:[MessageSchema]
+ })
